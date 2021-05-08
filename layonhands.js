4ce45a3e273680a1f3ddfa6b2c4805d81785d841
darklizzard <45498804+darklizzard@users.noreply.github.com>
2021-05-08 10:18:52 -0400

    update color

--- layonhands.js
+++ layonhands.js
@@ -1,21 +1,19 @@
 !scriptcard  {{ 
     --#title|Lay On Hands
     --#emoteText|@{selected|token_name} heals @{target|token_name}
-    --#titleFontColor|#FFFFFF
-    --#titleCardBackground|#4664b4
+    --#titleFontColor|#7E2d40
+    --#titleCardBackground|#dcc21d
     --#TitleFontFace|New Century Schoolbook
-    --#leftsub|Long Range
-    --#rightsub|50' / 100'
-    --#oddrowbackground|#d4cebe
-    --#evenrowbackground|#beb49c
+    --#leftsub|Action
+    --#rightsub|Touch
+    --#oddRowBackground|#eeeeee
+    --#evenRowBackground|#ffffff
     --#sourceToken|@{selected|token_id}
     --#targetToken|@{target|token_id}
     --=ResourceAvail|?{Lay on Hands - @{selected|class_resource} points available}
-    --@alter| _target|@{target|token_id} _bar|1 _amount|+[$ResourceAvail]
+    --@token-mod|_ids @{target|token_id} _set bar1_value|+[$ResourceAvail]
     --@modbattr| _silent _name @{selected|character_name} _class_resource|-[$ResourceAvail]
-    --+Description|Your blessed touch can heal wounds. You have a pool of healing power that replenishes when you take a long rest. With that pool, you can restore a total number of hit points equal to your paladin level × 5.
-    As an action, you can touch a creature and draw power from the pool to restore a number of hit points to that creature, up to the maximum amount remaining in your pool.
-    Alternatively, you can expend 5 hit points from your pool of healing to cure the target of one disease or neutralize one poison affecting it. You can cure multiple diseases and neutralize multiple poisons with a single use of Lay on Hands, expending hit points separately for each one.
-    This feature has no effect on undead and constructs.
+    --@roll20AM|_audio,play,nomenu|Cure Light Wounds Spell by Pablo Betancourt
+    --+|@{target|token_name} regains [$ResourceAvail] hit points.
      
     }}
